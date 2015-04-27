bce137b61abdb20621934fe5c5aecb3c04582287
Jean Bordat <bordat.jean@gmail.com>
2015-04-27 11:03:29 +0200

    startup update
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 set -e
 usermod --uid $UID minecraft
+#chown -R minecraft /data /start-minecraft.sh
 chown -R minecraft /data /start-minecraft
+chmod 755 /start-minecraft
 
-exec sudo -E -u minecraft /start-minecraft
-
+#exec sudo -E -u minecraft /start-minecraft.sh
+exec su -s /bin/bash -c /start-minecraft minecraft
